0ee6f1551b32677393ec04160781a0fddd912d31
daKhosa <54072134+daKhosa@users.noreply.github.com>
2025-10-26 01:13:56 +0000

    fix: Add missing HashMap import in MineflayerBotManager

--- src/main/java/com/mineagents/sensors/botmanager/MineflayerBotManager.java
+++ src/main/java/com/mineagents/sensors/botmanager/MineflayerBotManager.java
@@ -7,6 +7,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
